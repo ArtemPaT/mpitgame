a4a1b41cd9116cf3d05e659b2de6581b886da3c9
ArtemPaT <60203224+ArtemPaT@users.noreply.github.com>
2022-04-21 16:15:58 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+	<style type="text/css">
+		@font-face {
+			font-family: code;
+			src: url(Coda-Regular.ttf);
+		}
+		.code {
+			font-family: code;
+		}
+		.bg-black {
+			background: black;
+		}
+		.bg-kek {
+			background: #2AC367;
+		}
+	</style>
+</head>
+<body style="font-family: code;background: linear-gradient(180deg, #027200 0%, rgba(0, 80, 91, 0.54) 100%); height: 1000px;">
+	<nav class="navbar bg-dark p-0 text-light" style="height: 250px; background: linear-gradient(180deg, black,#18343d);">
+		<div class="rounded-circle bg-kek" style="height: 16px; width: 16px; position: absolute; left: 50px; top:50px;">
+			
+		</div>
+		<div class="rounded-circle bg-kek" style="height: 24px; width: 24px; position: absolute; left: 300px; top:45px;">
+			
+		</div>
+		<div class="rounded-circle bg-kek" style="height: 32px; width: 32px; position: absolute; left: 130px; top:160px;">
+			
+		</div>
+		<div class="rounded-circle bg-kek" style="height: 16px; width: 16px; position: absolute; left: 1430px; top:50px;">
+			
+		</div>
+		<div class="rounded-circle bg-kek" style="height: 24px; width: 24px; position: absolute; left: 1200px; top:45px;">
+			
+		</div>
+		<div class="rounded-circle bg-kek" style="height: 32px; width: 32px; position: absolute; left: 1300px; top:160px;">
+			
+		</div>
+		<div class="col-12">
+			<h1 class="text-center"><span class="mr-5"><</span>Helping Hand<span class="ml-5">></h1>
+		</div>
+	</nav>
+	<div class="col-11 mx-auto bg-black p-0" style="height: 600px;">
+		<div class="col-12" style="height: 200px; padding-top: 70px">
+			<h1 class="text-success text-center">We'll always lend a helping hand</h1>
+		</div>
+		<div class="col-12 p-1">
+			<div class="col-12 bg-success text-white pt-4" style="height: 400px;">
+				<h1 class="">bluonjdfjiofdaijoadfjiofadjoi</h1>
+				<br><br>
+				<div class="d-flex">
+					<div class="col-5 ml-5 bg-black" style="border: 4px solid lime; height: 200px;">
+						<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.  </h3>
+						<button class="btn" style="background: lime">Start your project now!</button>
+					</div>
+					<div class="col-5 ml-5 bg-black" style="border: 4px solid lime; height: 200px;">
+						<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.  </h3>
+						<button class="btn " style="background: lime">Start your project now!</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+</body>
+</html>
